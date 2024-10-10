c680cf27b8be28110088e30ceef7408f57455582
LekoArts <lekoarts@gmail.com>
2024-10-10 22:36:50 +0200

    feat: Add RSS feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,33 @@
+import type { RSSFeedItem } from '@astrojs/rss'
+import rss from '@astrojs/rss'
+import { SITE } from '@constants/meta'
+import type { APIRoute } from 'astro'
+import { getCollection } from 'astro:content'
+import { filterPublished } from '@utils/collection'
+import { slugify } from '@utils/slugify'
+
+export const GET: APIRoute = async () => {
+	const writing = (await getCollection('writing', filterPublished)).map(entry => ({
+		title: entry.data.title,
+		description: entry.data.description,
+		link: slugify(entry.slug, entry.data.category.id),
+		pubDate: entry.data.date,
+	} satisfies RSSFeedItem))
+
+	const garden = (await getCollection('garden')).map(entry => ({
+		title: entry.data.title,
+		description: entry.data.description,
+		link: slugify(entry.slug, 'garden'),
+		pubDate: entry.data.date,
+	} satisfies RSSFeedItem))
+
+	const items = [...writing, ...garden].sort((a, b) => new Date(b.pubDate).getTime() - new Date(a.pubDate).getTime())
+
+	return rss({
+		title: SITE.titleDefault,
+		description: SITE.description,
+		site: SITE.url,
+		items,
+		customData: '<language>en-us</language>',
+	})
+}
