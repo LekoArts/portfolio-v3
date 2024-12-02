6198cfb40ad182747c90362cd222239ec9d074c1
LekoArts <lekoarts@gmail.com>
2024-12-02 22:02:13 +0100

    fix: Style specificity for theme toggle

--- src/components/blocks/theme-toggle.css.ts
+++ src/components/blocks/theme-toggle.css.ts
@@ -1,19 +1,20 @@
 import { globalStyle, style } from '@vanilla-extract/css'
-import { pseudoSelectors } from '@styles/selectors'
 import { vars } from '@styles/themes/contract.css'
 import { colorPalette } from '@styles/tokens/colors'
 
 export const toggleIconStyle = style({
+})
+
+globalStyle(`#lekoarts-theme-select .${toggleIconStyle}`, {
 	color: vars.color.textMuted,
 	transition: 'all 0.3s ease-in-out',
 	borderRadius: vars.borderRadius.full,
 	padding: 0,
 	fontSize: vars.fontSize.md,
-	selectors: {
-		[pseudoSelectors.hover]: {
-			color: vars.color.heading,
-		},
-	},
+})
+
+globalStyle(`#lekoarts-theme-select .${toggleIconStyle}:hover`, {
+	color: vars.color.heading,
 })
 
 globalStyle(`div[data-variant-name="fullBleed"] .${toggleIconStyle}`, {
