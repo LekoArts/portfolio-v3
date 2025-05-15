8b852c10bd2e90912f63972544e4f78a4c0904cd
LekoArts <lekoarts@gmail.com>
2025-05-15 13:07:49 +0900

    fix: Use rem for media queries

--- src/styles/tokens/breakpoints.ts
+++ src/styles/tokens/breakpoints.ts
@@ -1,12 +1,16 @@
 export const breakpointNames = ['mobile', 'sm', 'md', 'lg', 'xl', '2xl'] as const
 
+/**
+ * For accessbility reasons the media queries are defined in "rem" unit.
+ * The default (unchanged) font-size is 16px, so 1rem = 16px.
+ */
 export const breakpoints = {
-	'mobile': 0,
-	'sm': 640,
-	'md': 768,
-	'lg': 1024,
-	'xl': 1280,
-	'2xl': 1536,
+	'mobile': 0, // 0px
+	'sm': 40, // 640px
+	'md': 48, // 768px
+	'lg': 64, // 1024px
+	'xl': 80, // 1280px
+	'2xl': 96, // 1536px
 } as const
 
 // Get number values from breakpoints and exclude "mobile"
@@ -16,5 +20,5 @@ export type Breakpoint = keyof typeof breakpoints
 export type BreakpointValues = (typeof breakpoints)[Breakpoint]
 
 export function minMediaQuery(breakpoint: Exclude<Breakpoint, 'mobile'>) {
-	return `screen and (min-width: ${breakpoints[breakpoint]}px)`
+	return `screen and (min-width: ${breakpoints[breakpoint]}rem)`
 }
